0f76ba8b40017b98ea90dfa10f0493ab9e0dfcba
hicham <hishamo@outlook.com>
2024-11-12 11:02:23 -0800

    Complete manual-linux script

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -5,6 +5,7 @@
 set -e
 set -u
 
+BASEDIR=/home/wifiez/coursera/linuxSysProg/assignment-2-hishamo76/finder-app/
 OUTDIR=/tmp/aeld
 KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
 KERNEL_VERSION=v5.15.163
@@ -35,6 +36,10 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- make -j8 mrproper
+    ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- make -j8 defconfig
+    ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- make -j4 all
+    # make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- dtb 
 fi
 
 echo "Adding the Image in outdir"
@@ -48,6 +53,12 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir "${OUTDIR}/rootfs"
+cd "${OUTDIR}/rootfs"
+
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log 
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -56,25 +67,71 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    echo "distclean...."
+    make distclean
+    make defconfig
 else
+    echo "Going into busy box"
     cd busybox
 fi
 
 # TODO: Make and install busybox
 
+
+
+make distclean
+make defconfig
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}  ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install 
+
+
+cd ${OUTDIR}/rootfs
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+program_interpreter=$(${CROSS_COMPILE}readelf -a ${OUTDIR}/bin/busybox | grep "program interpreter" | awk -F: '{print $2}' | awk '{print $1}')
+shared_libraries=$(${CROSS_COMPILE}readelf -a ${OUTDIR}/bin/busybox | grep "Shared library" | awk -F: '{print $2}' | awk '{print $1}')
+
+echo "${program_interpreter}"
+echo "${shared_libraries}"
 
 # TODO: Add library dependencies to rootfs
+cp /home/wifiez/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 /tmp/aeld/rootfs/lib
+
+cp /home/wifiez/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64
+cp /home/wifiez/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64
+cp /home/wifiez/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64
+echo "Finished copying libs"
 
 # TODO: Make device nodes
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 600 dev/console c 5 1
 
 # TODO: Clean and build the writer utility
+if [ -e ${BASEDIR}/writer ]; then
+    rm ${BASEDIR}/writer
+fi
+cd ${BASEDIR}
+echo "${CROSS_COMPILE}gcc -o writer writer.c"
+${CROSS_COMPILE}gcc -o writer writer.c
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp ${BASEDIR}/writer ${OUTDIR}/rootfs/home
+
+cp ${BASEDIR}/finder.sh ${OUTDIR}/rootfs/home
+cp ${BASEDIR}/finder-test.sh ${OUTDIR}/rootfs/home
+
+mkdir -p ${OUTDIR}/rootfs/home/conf
+cp -r ${BASEDIR}/conf/ ${OUTDIR}/rootfs/home
+
+
+cp ${BASEDIR}/autorun-qemu.sh ${OUTDIR}/rootfs/home
+
 
 # TODO: Chown the root directory
+cd "${OUTDIR}/rootfs" 
+sudo chown -R root:root *
 
 # TODO: Create initramfs.cpio.gz
+find . |  cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+gzip -f ${OUTDIR}/initramfs.cpio
+ls ${OUTDIR}
